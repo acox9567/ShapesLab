f00eed1a7146d170281147f04a872f692697cec1
BT_1N3_17@bktech.local <BT_1N3_17@bktech.local>
2018-10-01 10:15:51 -0400

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,21 @@
+//Alexander Cox
+//Monday, October 1, 2018
+
+public abstract class Shape {
+
+    private String type;
+
+    public Shape(String type){
+
+        this.type = type;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public String toString() {
+
+        return "I am a " + type + ".";
+    }
+}
