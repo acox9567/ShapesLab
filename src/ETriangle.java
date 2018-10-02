f47585c8da5383b7f179e02822a0f42224dc34f2
BT_1N3_16 <bsmith3816@bths.edu>
2018-10-02 09:48:54 -0400

    10/2/18 Brandon

---
+++ src/ETriangle.java
@@ -0,0 +1,20 @@
+
+public class ETriangle extends Shape
+{
+    private int side;
+
+    public ETriangle(int side)
+    {
+        super("ETriangle");
+        this.side = side;
+    }
+    @Override
+    public double getArea(){
+        return Math.sqrt(3) * 0.25 * (side * side);
+    }
+
+    @Override
+    public double getPerimeter(){
+        return side * 3;
+    }
+}
